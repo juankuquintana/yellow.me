062280b9a0f7c098def43d963230f067f9e898ef
Marco Chavez <marco@yellowme.mx>
2020-08-12 02:26:32 -0500

    change Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,28 +7,11 @@ import MediumIcon from "../images/medium-icon.svg"
 
 const Footer = () => {
   return (
-    <div className="full-screen content-ctr">
-      <h2 className="fw-200">
-        Don't be shy, <br />
-        <div className="fw-300">say hi.</div>
-      </h2>
-      <h2>👋</h2>
-      <h2 className="footer--email">hi@yellow.me</h2>
+    <div className="footer content-ctr">
+      <div className="divider" />
 
-      <h5 className="fw-500">Mérida</h5>
-      <p>
-        Baja California 284 p.8 <br />
-        Hipódromo, 06100
-      </p>
-
-      <h5 className="fw-500">Mexico City</h5>
-      <p>
-        Baja California 284 p.8 <br />
-        Hipódromo, 06100
-      </p>
-
-      <div className="footer--follow-us fw-500">
-        Follow us
+      <div className="footer--find-us fw-700">
+        Find us
       </div>
       <div className="footer--social-icons">
         <img src={TtIcon} />
