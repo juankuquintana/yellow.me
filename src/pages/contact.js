1ce69308481bab6d76aef753fdd8a4989de247f2
Marco Chavez <marco@yellowme.mx>
2020-08-17 19:11:05 -0500

    implement google sheets api to save emails newsletter and contact form requests

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,12 +13,24 @@ import ContactIcon2 from "../images/contact/contact-icon-2.png"
 import DesignerIcon from "../images/contact/Designer.png"
 import EngineerIcon from "../images/contact/Engineer.png"
 import OtherIcon from "../images/contact/Other.png"
+import ConfirmationIcon from "../images/contact/Confirmation.png"
 
 const ContactPage = () => {
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [selectedOption, setSelectedOption] = useState("Designer");
   const [selectInputExpand, setSelectInputExpand] = useState(false)
   const [yearsExperience, setYearsExperience] = useState("Select your experience");
+  const [emailSent, setEmailSent] = useState(false);
+
+  const typeName = (event) => {
+    setName(event.target.value);
+  };
+
+  const typeEmail = (event) => {
+    setEmail(event.target.value);
+  };
 
   const handleFormSubmit = formSubmitEvent => {
     formSubmitEvent.preventDefault();
@@ -36,105 +48,142 @@ const ContactPage = () => {
     setSelectInputExpand(!selectInputExpand);
   }
 
+
+  const updateSheetValues = () => {
+
+    let SHEET_ID = process.env.SHEET_ID;
+    let ACCESS_TOKEN = process.env.ACCESS_TOKEN_SHEET;
+
+    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Talent!A2:D2:append?valueInputOption=USER_ENTERED`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        //update this token with yours. 
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+      body: JSON.stringify(
+        {
+          "range": "Talent!A2:D2",
+          "majorDimension": "ROWS",
+          "values": [
+            [name, email, selectedOption, yearsExperience],
+          ],
+        }
+      )
+    }).then(()=>{
+      setEmailSent(true);
+    })
+  }
+  
+
   return(
     <Layout>
-      <div className="client-card content-ctr">
-        <h2>Tell us, we are all ears</h2>
-        <div className="container">
-          <div className="subscribe">
-            <div>
-              <img src={ContactIcon1}/>
-            </div>
+      <div className={`${emailSent ? 'hidden' : ''}`}>
+        <div className="client-card content-ctr">
+          <h2>Tell us, we are all ears</h2>
+          <div className="container">
+            <div className="subscribe">
+              <div>
+                <img src={ContactIcon1}/>
+              </div>
 
-            <span>Do you need support building or extending a product team?</span>
-            <p>Schedule a discovery call</p>
+              <span>Do you need support building or extending a product team?</span>
+              <p>Schedule a discovery call</p>
 
-            <div className="program-a-call">
-              <span className="uppercase">Program a call</span>
-              <img src={SendIcon} />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="talent-card content-ctr">
-        <div className="container">
-          <div className="subscribe">
-            <div>
-              <img src={ContactIcon2}/>
-            </div>
-
-            <span>Are you open to join us and work on exciting Silicon Valley level companies?</span>
-
-            <div className="contact-form">
-            
-              <p>Tell us your name</p>
-              <input type="text" name="name"/>
-
-              <p>What’s your email?</p>
-              <input type="email" name="email"/>
-
-              <p>And you are?</p>
-              <form className="form-job-searched" onSubmit={handleFormSubmit}>
-                <input className="checkbox-tools" type="radio" value="Designer" checked={selectedOption === "Designer"} />
-                  <label className="for-checkbox-tools" onClick={() => handleOptionChange("Designer")}>
-                    <div className="radio-button-inside">
-                      <img src={DesignerIcon}/>
-                      <p>Designer</p>
-                    </div>
-                  </label>
-
-                <input className="checkbox-tools" type="radio" value="Engineer" checked={selectedOption === "Engineer"} />
-                  <label className="for-checkbox-tools" onClick={() => handleOptionChange("Engineer")}>
-                    <div className="radio-button-inside">
-                      <img src={EngineerIcon}/>
-                      <p>Engineer</p>
-                    </div>
-                  </label>
-
-                <input className="checkbox-tools" type="radio" value="Other" checked={selectedOption === "Other"} />
-                  <label className="for-checkbox-tools" onClick={() => handleOptionChange("Other")}>
-                    <div className="radio-button-inside">
-                      <img src={OtherIcon}/>
-                      <p>Other</p>
-                    </div>
-                  </label>
-              </form>
-
-              <p>Paid professional experience</p>
-              <form className="form-years-experience" onSubmit={handleFormSubmit}>
-
-              <span className={`dropdown ${selectInputExpand ? 'expanded': ''}`} onClick={selectInputToogle}>
-
-                <label>{yearsExperience}</label>
-
-                <input type="radio" value="1 to 3 years" checked={yearsExperience === "1 to 3 years"}/>
-                <label onClick={()=> {handleYearsExperienceChange("1 to 3 years")}}>1 to 3 years</label>
-
-                <input type="radio" value="3 to 5 years" checked={yearsExperience === "3 to 5 years"}/>
-                <label onClick={()=> {handleYearsExperienceChange("3 to 5 years")}}>3 to 5 years</label>
-
-                <input type="radio" value="5 to 8 years" checked={yearsExperience === "5 to 8 years"}/>
-                <label onClick={()=> {handleYearsExperienceChange("5 to 8 years")}}>5 to 8 years</label>
-
-                <input type="radio" value="+ 8 years" checked={yearsExperience === "+ 8 years"}/>
-                <label onClick={()=> {handleYearsExperienceChange("+ 8 years")}}>+ 8 years</label>
-              </span>
-
-              </form>
-
-            </div>
-
-
-            <div className="program-a-call">
-              <span className="uppercase">Send form</span>
-              <img src={SendIcon} />
+              <div className="program-a-call">
+                <span className="uppercase">Program a call</span>
+                <a href="https://calendly.com/cesarsalazar "><img src={SendIcon} /></a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <Footer />
+        <div className="talent-card content-ctr">
+          <div className="container">
+            <div className="subscribe">
+              <div>
+                <img src={ContactIcon2}/>
+              </div>
+
+              <span>Are you open to join us and work on exciting Silicon Valley level companies?</span>
+
+              <div className="contact-form">
+              
+                <p>Tell us your name</p>
+                <input type="text" name="name" onChange={typeName}/>
+
+                <p>What’s your email?</p>
+                <input type="email" name="email" onChange={typeEmail}/>
+
+                <p>And you are?</p>
+                <form className="form-job-searched" onSubmit={handleFormSubmit}>
+                  <input className="checkbox-tools" type="radio" value="Designer" checked={selectedOption === "Designer"} />
+                    <label className="for-checkbox-tools" onClick={() => handleOptionChange("Designer")}>
+                      <div className="radio-button-inside">
+                        <img src={DesignerIcon}/>
+                        <p>Designer</p>
+                      </div>
+                    </label>
+
+                  <input className="checkbox-tools" type="radio" value="Engineer" checked={selectedOption === "Engineer"} />
+                    <label className="for-checkbox-tools" onClick={() => handleOptionChange("Engineer")}>
+                      <div className="radio-button-inside">
+                        <img src={EngineerIcon}/>
+                        <p>Engineer</p>
+                      </div>
+                    </label>
+
+                  <input className="checkbox-tools" type="radio" value="Other" checked={selectedOption === "Other"} />
+                    <label className="for-checkbox-tools" onClick={() => handleOptionChange("Other")}>
+                      <div className="radio-button-inside">
+                        <img src={OtherIcon}/>
+                        <p>Other</p>
+                      </div>
+                    </label>
+                </form>
+
+                <p>Paid professional experience</p>
+                <form className="form-years-experience" onSubmit={handleFormSubmit}>
+
+                <span className={`dropdown ${selectInputExpand ? 'expanded': ''}`} onClick={selectInputToogle}>
+
+                  <label>{yearsExperience}</label>
+
+                  <input type="radio" value="1 to 3 years" checked={yearsExperience === "1 to 3 years"}/>
+                  <label onClick={()=> {handleYearsExperienceChange("1 to 3 years")}}>1 to 3 years</label>
+
+                  <input type="radio" value="3 to 5 years" checked={yearsExperience === "3 to 5 years"}/>
+                  <label onClick={()=> {handleYearsExperienceChange("3 to 5 years")}}>3 to 5 years</label>
+
+                  <input type="radio" value="5 to 8 years" checked={yearsExperience === "5 to 8 years"}/>
+                  <label onClick={()=> {handleYearsExperienceChange("5 to 8 years")}}>5 to 8 years</label>
+
+                  <input type="radio" value="+ 8 years" checked={yearsExperience === "+ 8 years"}/>
+                  <label onClick={()=> {handleYearsExperienceChange("+ 8 years")}}>+ 8 years</label>
+                </span>
+
+                </form>
+
+              </div>
+
+
+              <div className="program-a-call">
+                <span className="uppercase">Send form</span>
+                <img src={SendIcon} onClick={updateSheetValues}/>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+      
+
+      <div className={`confirmation-email df-cc df-column content-ctr ${emailSent ? '' : 'hidden'}`}>
+        <h2 className="">Thank you!</h2>
+        <img src={ConfirmationIcon}></img>
+        <p>We will send you a confirmation email</p>
+      </div>
 
     </Layout>  
   )
--- src/pages/culture.js
+++ src/pages/culture.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -8,130 +8,168 @@ import ValuesList from "../components/ValuesList"
 import Yellows from "../components/Yellows";
 
 import SendIcon from "../images/button-send.svg"
+import SendSendedIcon from "../images/button-sended.svg"
 
 import Footer from "../components/Footer"
 
 import NewsletterIcon from "../images/culture/newsletter-icon.png"
 
-const CulturePage = () => (
-  <Layout>
+const CulturePage = () => {
+
+  const [email, setEmail] = useState("");
+  const [emailSent, setEmailSent] = useState(false);
+
+  const typeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const sendEmail = () => {
+
+    let SHEET_ID = process.env.SHEET_ID;
+    let ACCESS_TOKEN = process.env.ACCESS_TOKEN_SHEET;
+
+    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Newsletter!A2:A3:append?valueInputOption=USER_ENTERED`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        //update this token with yours. 
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+      body: JSON.stringify(
+        {
+          "range": "Newsletter!A2:A3",
+          "majorDimension": "ROWS",
+          "values": [
+            [email],
+          ],
+        }
+      )
+    }).then(()=>{
+      setEmailSent(true);
+    })
+  }
+
+  return (
+    <Layout>
     
-    <div className="join-a-culture-section df-cc df-column content-ctr">
-      <h2 className="">Join a culture of permanent <br/> work-in-progress.</h2>
-    </div>
-
-    <div className="photos-grid-a">
-      <div className="photos-wrapper-a">
-        <div class="slot photo-1"></div>
-        <div class="slot photo-2"></div>
-        <div class="slot photo-3"></div>
-        <div class="slot photo-4"></div>
-        <div class="slot photo-5"></div>
-        <div class="slot photo-6"></div>
+      <div className="join-a-culture-section df-cc df-column content-ctr">
+        <h2 className="">Join a culture of permanent <br/> work-in-progress.</h2>
       </div>
-    </div>
 
-    <div className="principles-section">
-      <div className="section-1 content-ctr">
-        <div className="section-1--left">
-          <h6 className="statement">
-          Over the last 7 years, we’ve been learning from our wins and losses. 
-          Every person and experience have shaped our culture, and we expect that to continue forever.
-          </h6>
-        </div>
-
-        <div className="section-1--right">
-          <h2>Every voice matters.</h2>
-          <p>
-          We don’t believe hierarchies mean much in our industry. 
-          At Yellowme we are a community of committed designers and engineers with a shared value system. 
-          We reject bureaucracy and believe in simple and thoughtful processes.
-          </p>
-          <div className="divider" />
-
-          <h2>Be a leader.</h2>
-          <p>
-          Leadership is not management. Leadership starts with one’s self and never ends. 
-          A true leader inspires their peers, apprentices, other leaders and the community at large.
-          </p>
-          <div className="divider" />
-
-          <h2>Commit wholeheartedly.</h2>
-          <p>
-          We don’t simply place you on a project. 
-          Yellowme makes sure that you are fully committed to the purpose of the client you’re working with and deeply care about the challenge ahead.
-          </p>
-          <div className="divider" />
-
-          <h2>Become one with our clients.</h2>
-          <p>
-          We are not an agency or a dev shop. We build real world teams. 
-          This means putting the work alongside our clients to establish the tools, process, mindset and camaraderie that make a group successful in the long run.
-          </p>
+      <div className="photos-grid-a">
+        <div className="photos-wrapper-a">
+          <div class="slot photo-1"></div>
+          <div class="slot photo-2"></div>
+          <div class="slot photo-3"></div>
+          <div class="slot photo-4"></div>
+          <div class="slot photo-5"></div>
+          <div class="slot photo-6"></div>
         </div>
       </div>
-    </div>
 
-    <div className="photos-grid-b">
-      <div className="photos-wrapper-b">
-        <div class="slot photo-1"></div>
-        <div class="slot photo-2"></div>
-        <div class="slot photo-3"></div>
-        <div class="slot photo-4"></div>
-        <div class="slot photo-5"></div>
-      </div>
-    </div>
-
-    <div className="we-are-our-values-section">
-      <div className="section-1 content-ctr">
-        <div className="section-1--left">
-          <h2 className="statement">We are our values</h2>
-        </div>
-
-        <div className="section-1--right">
-          <p>
-          The team has grown, our model has continuously evolved but our core values remain the same. 
-          If anything, we’re always looking to strengthen them by bringing the best possible people to work with us.</p>
-        </div>
-      </div>
-    </div>
-    
-    <ValuesList />
-
-    <div>
-      <div className="meet-yellows-section content-ctr-thin">
-        <h2>Meet the Yellows</h2>
-          <Yellows />
-      </div>
-    </div>
-
-    <div>
-      <div className="subscribe-section df-cc df-column content-ctr">
-        <h2>We are constantly posting new <br/>job opportunities</h2>
-        <div className="subscribe">
-
-          <div>
-            <img src={NewsletterIcon} />
+      <div className="principles-section">
+        <div className="section-1 content-ctr">
+          <div className="section-1--left">
+            <h6 className="statement">
+            Over the last 7 years, we’ve been learning from our wins and losses. 
+            Every person and experience have shaped our culture, and we expect that to continue forever.
+            </h6>
           </div>
 
-          <div className="form-email">
-            <span>Subscribe to our job opportunity feed</span>
+          <div className="section-1--right">
+            <h2>Every voice matters.</h2>
+            <p>
+            We don’t believe hierarchies mean much in our industry. 
+            At Yellowme we are a community of committed designers and engineers with a shared value system. 
+            We reject bureaucracy and believe in simple and thoughtful processes.
+            </p>
+            <div className="divider" />
+
+            <h2>Be a leader.</h2>
+            <p>
+            Leadership is not management. Leadership starts with one’s self and never ends. 
+            A true leader inspires their peers, apprentices, other leaders and the community at large.
+            </p>
+            <div className="divider" />
+
+            <h2>Commit wholeheartedly.</h2>
+            <p>
+            We don’t simply place you on a project. 
+            Yellowme makes sure that you are fully committed to the purpose of the client you’re working with and deeply care about the challenge ahead.
+            </p>
+            <div className="divider" />
+
+            <h2>Become one with our clients.</h2>
+            <p>
+            We are not an agency or a dev shop. We build real world teams. 
+            This means putting the work alongside our clients to establish the tools, process, mindset and camaraderie that make a group successful in the long run.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="photos-grid-b">
+        <div className="photos-wrapper-b">
+          <div class="slot photo-1"></div>
+          <div class="slot photo-2"></div>
+          <div class="slot photo-3"></div>
+          <div class="slot photo-4"></div>
+          <div class="slot photo-5"></div>
+        </div>
+      </div>
+
+      <div className="we-are-our-values-section">
+        <div className="section-1 content-ctr">
+          <div className="section-1--left">
+            <h2 className="statement">We are our values</h2>
+          </div>
+
+          <div className="section-1--right">
+            <p>
+            The team has grown, our model has continuously evolved but our core values remain the same. 
+            If anything, we’re always looking to strengthen them by bringing the best possible people to work with us.</p>
+          </div>
+        </div>
+      </div>
+      
+      <ValuesList />
+
+      <div>
+        <div className="meet-yellows-section content-ctr-thin">
+          <h2>Meet the Yellows</h2>
+            <Yellows />
+        </div>
+      </div>
+
+      <div>
+        <div className="subscribe-section df-cc df-column content-ctr">
+          <h2>We are constantly posting new <br/>job opportunities</h2>
+          <div className="subscribe">
+
             <div>
-              <input className="email-input" type="text" name="email" placeholder="Your email here"/>
-              <div className="send-email">
-                <span>We send one email per week maximum</span>
-                <img src={SendIcon} />
+              <img src={NewsletterIcon} />
+            </div>
+
+            <div className="form-email">
+              <span>Subscribe to our job opportunity feed</span>
+              <div>
+                <span className={`suscribed ${emailSent ? '' : 'hidden'}`}>You’ve been suscribed! 🥳</span>
+                <input className={`email-input ${emailSent ? 'hidden' : ''}`} type="text" name="email" placeholder="Your email here" onChange={typeEmail}/>
+                <div className="send-email">
+                  <span>We send one email per week maximum</span>
+                  <img src={!emailSent ? SendIcon : SendSendedIcon} onClick={sendEmail}/>
+                </div>
               </div>
             </div>
-          </div>
 
+          </div>
         </div>
       </div>
-    </div>
 
-    <div className="culture-footer"><Footer /></div>
+      <div className="culture-footer"><Footer /></div>
 
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default CulturePage
